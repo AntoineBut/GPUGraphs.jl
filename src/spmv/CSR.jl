cbbdad3d6b90cad0a6d3951aae3fd1d937921b2e
AntoineBut <antoine.buttier@epfl.ch>
2025-11-17 18:14:51 +0100

    added ranges for CSR

--- src/spmv/CSR.jl
+++ src/spmv/CSR.jl
@@ -1,3 +1,33 @@
+
+function gpu_spmv!(
+    C::ResVec,
+    A::SparseGPUMatrixCSR{Tv,Ti},
+    B::InputVec;
+    mul::Function = GPUGraphs_mul,
+    add::Function = GPUGraphs_add,
+    accum::Function = GPUGraphs_second,
+    range::Union{Nothing,UnitRange} = nothing,
+    mask::Union{Nothing,AbstractVector} = nothing,
+) where {
+    Tv,
+    Ti<:Integer,
+    ResType<:Number,
+    InputType<:Number,
+    ResVec<:AbstractVector{ResType},
+    InputVec<:AbstractVector{InputType},
+}
+    # call appropriate specialized function
+    if mask === nothing && range === nothing
+        _simple_gpu_spmv!(C, A, B, mul, add, accum)
+    elseif mask !== nothing && range === nothing
+        _dense_mask_gpu_spmv!(C, A, B, mul, add, accum, mask)
+    elseif mask === nothing && range !== nothing
+        _range_gpu_spmv!(C, A, B, mul, add, accum, range)
+    else
+        _range_mask_gpu_spmv!(C, A, B, mul, add, accum, mask, range)
+    end
+end
+
 @kernel function csr_spmv_kernel!(
     c,
     @Const(a_row_ptr),
@@ -5,7 +35,7 @@
     @Const(a_nz_val),
     @Const(b),
     @Const(monoid_neutral_element),
-    @Const(terminal_value),
+    @Const(monoid_absorb),
     mul,
     add,
     accum,
@@ -16,12 +46,46 @@
     for i = a_row_ptr[row]:(a_row_ptr[row+1]-1)
         col = a_col_val[i]
         acc = add(acc, mul(a_nz_val[i], b[col], row, col, col, 1), row, col, col, 1)
-        if acc == terminal_value
+        if acc == monoid_absorb
             break
         end
     end
     c[row] = accum(c[row], acc, row, 1, row, 1)
 end
+
+function _simple_gpu_spmv!(
+    C::ResVec,
+    A::SparseGPUMatrixCSR{Tv,Ti},
+    B::InputVec,
+    mul::Function,
+    add::Function,
+    accum::Function,
+) where {
+    Tv,
+    Ti<:Integer,
+    ResType<:Number,
+    InputType<:Number,
+    ResVec<:AbstractVector{ResType},
+    InputVec<:AbstractVector{InputType},
+}
+    _validate_args(C, A, B, nothing)
+    backend = get_backend(A)
+    csr_spmv_kernel!(backend)(
+        C,
+        A.rowptr,
+        A.colval,
+        A.nzval,
+        B,
+        monoid_neutral(promote_type(Tv, InputType), add),
+        monoid_absorb(promote_type(Tv, InputType), add),
+        mul,
+        add,
+        accum;
+        ndrange = size(C, 1),
+    )
+    return
+end
+
 
 @kernel function sparse_masked_csr_spmv_kernel!(
     c,
@@ -30,7 +94,7 @@ end
     @Const(a_nz_val),
     @Const(b),
     @Const(monoid_neutral_element),
-    @Const(terminal_value),
+    @Const(monoid_absorb),
     @Const(mask),
     mul,
     add,
@@ -43,7 +107,7 @@ end
     for i = a_row_ptr[row]:(a_row_ptr[row+1]-1)
         col = a_col_val[i]
         acc = add(acc, mul(a_nz_val[i], b[col], row, col, col, 1), row, col, col, 1)
-        if acc == terminal_value
+        if acc == monoid_absorb
             break
         end
     end
@@ -57,7 +121,7 @@ end
     @Const(a_nz_val),
     @Const(b),
     @Const(monoid_neutral_element),
-    @Const(terminal_value),
+    @Const(monoid_absorb),
     @Const(mask),
     @Const(mask_zero),
     mul,
@@ -71,13 +135,14 @@ end
         for i = a_row_ptr[row]:(a_row_ptr[row+1]-1)
             col = a_col_val[i]
             acc = add(acc, mul(a_nz_val[i], b[col], row, col, col, 1), row, col, col, 1)
-            if acc == terminal_value
+            if acc == monoid_absorb
                 break
             end
         end
         c[row] = accum(c[row], acc, row, 1, row, 1)
     end
 end
+
 
 @kernel function any_dense_masked_csr_spmv_kernel!(
     c,
@@ -105,14 +170,159 @@ end
     end
 end
 
-function gpu_spmv!(
+function _dense_mask_gpu_spmv!(
     C::ResVec,
     A::SparseGPUMatrixCSR{Tv,Ti},
-    B::InputVec;
-    mul::Function = GPUGraphs_mul,
-    add::Function = GPUGraphs_add,
-    accum::Function = GPUGraphs_second,
-    mask::Union{MaskVec,Nothing} = nothing,
+    B::InputVec,
+    mul::Function,
+    add::Function,
+    accum::Function,
+    mask::AbstractVector,
+) where {
+    Tv,
+    Ti<:Integer,
+    ResType<:Number,
+    InputType<:Number,
+    ResVec<:AbstractVector{ResType},
+    InputVec<:AbstractVector{InputType},
+}
+    _validate_args(C, A, B, mask)
+    backend = get_backend(A)
+    if add == GPUGraphs_any #TODO: make a specialized any version for other kernels
+        any_dense_masked_csr_spmv_kernel!(backend)(
+            C,
+            A.rowptr,
+            A.colval,
+            A.nzval,
+            B,
+            mask,
+            zero(eltype(mask)),
+            mul,
+            accum;
+            ndrange = size(C, 1),
+        )
+        return
+    end
+    dense_masked_csr_spmv_kernel!(backend)(
+        C,
+        A.rowptr,
+        A.colval,
+        A.nzval,
+        B,
+        monoid_neutral(promote_type(Tv, InputType), add),
+        monoid_absorb(promote_type(Tv, InputType), add),
+        mask,
+        zero(eltype(mask)),
+        mul,
+        add,
+        accum;
+        ndrange = size(C, 1),
+    )
+    return
+end
+
+@kernel function range_csr_spmv_kernel!(
+    c,
+    @Const(a_row_ptr),
+    @Const(a_col_val),
+    @Const(a_nz_val),
+    @Const(b),
+    @Const(monoid_neutral_element),
+    @Const(monoid_absorb),
+    @Const(range_start),
+    mul,
+    add,
+    accum,
+)
+    # Computes A*B and stores the result in C
+    row = @index(Global, Linear) + range_start - 1
+    acc = monoid_neutral_element
+    for i = a_row_ptr[row]:(a_row_ptr[row+1]-1)
+        col = a_col_val[i]
+        acc = add(acc, mul(a_nz_val[i], b[col], row, col, col, 1), row, col, col, 1)
+        if acc == monoid_absorb
+            break
+        end
+    end
+    c[row] = accum(c[row], acc, row, 1, row, 1)
+end
+
+function _range_gpu_spmv!(
+    C::ResVec,
+    A::SparseGPUMatrixCSR{Tv,Ti},
+    B::InputVec,
+    mul::Function,
+    add::Function,
+    accum::Function,
+    range::UnitRange,
+) where {
+    Tv,
+    Ti<:Integer,
+    ResType<:Number,
+    InputType<:Number,
+    ResVec<:AbstractVector{ResType},
+    InputVec<:AbstractVector{InputType},
+}
+    _validate_args(C, A, B, nothing)
+    backend = get_backend(A)
+    range_csr_spmv_kernel!(backend)(
+        C,
+        A.rowptr,
+        A.colval,
+        A.nzval,
+        B,
+        monoid_neutral(promote_type(Tv, InputType), add),
+        monoid_absorb(promote_type(Tv, InputType), add),
+        range.start,
+        mul,
+        add,
+        accum;
+        ndrange = size(range, 1),
+    )
+    return
+end
+
+@kernel function range_masked_csr_spmv_kernel!(
+    c,
+    @Const(a_row_ptr),
+    @Const(a_col_val),
+    @Const(a_nz_val),
+    @Const(b),
+    @Const(monoid_neutral_element),
+    @Const(monoid_absorb),
+    @Const(mask),
+    @Const(mask_zero),
+    @Const(range_start),
+    mul,
+    add,
+    accum,
+)
+    # Computes A*B and stores the result in C
+    row = @index(Global, Linear) + range_start - 1
+    if mask[row] != mask_zero
+        acc = monoid_neutral_element
+        for i = a_row_ptr[row]:(a_row_ptr[row+1]-1)
+            col = a_col_val[i]
+            acc = add(acc, mul(a_nz_val[i], b[col], row, col, col, 1), row, col, col, 1)
+            if acc == monoid_absorb
+                break
+            end
+        end
+        c[row] = accum(c[row], acc, row, 1, row, 1)
+    end
+end
+
+
+
+function _range_mask_gpu_spmv!(
+    C::ResVec,
+    A::SparseGPUMatrixCSR{Tv,Ti},
+    B::InputVec,
+    mul::Function,
+    add::Function,
+    accum::Function,
+    mask::MaskVec,
+    range::UnitRange,
 ) where {
     Tv,
     Ti<:Integer,
@@ -123,7 +333,43 @@ function gpu_spmv!(
     InputVec<:AbstractVector{InputType},
     MaskVec<:AbstractVector{Tmask},
 }
-    # Computes A*B and stores the result in C
+    _validate_args(C, A, B, mask)
+    backend = get_backend(A)
+    range_masked_csr_spmv_kernel!(backend)(
+        C,
+        A.rowptr,
+        A.colval,
+        A.nzval,
+        B,
+        monoid_neutral(promote_type(Tv, InputType), add),
+        monoid_absorb(promote_type(Tv, InputType), add),
+        mask,
+        zero(eltype(mask)),
+        range.start,
+        mul,
+        add,
+        accum;
+        ndrange = size(range, 1),
+    )
+    return
+end
+
+
+function _validate_args(
+    C::ResVec,
+    A::SparseGPUMatrixCSR{Tv,Ti},
+    B::InputVec,
+    mask::Union{MaskVec,Nothing},
+) where {
+    Tv,
+    Ti<:Integer,
+    Tmask<:Integer,
+    ResType<:Number,
+    InputType<:Number,
+    ResVec<:AbstractVector{ResType},
+    InputVec<:AbstractVector{InputType},
+    MaskVec<:AbstractVector{Tmask},
+}
     # Check dimensions
     if size(A, 2) != length(B)
         throw(DimensionMismatch("Matrix dimensions must agree"))
@@ -131,97 +377,32 @@ function gpu_spmv!(
     if size(C, 1) != size(A, 1)
         throw(DimensionMismatch("Matrix dimensions must agree"))
     end
-    # Call the kernel
-    backend = get_backend(C)
-
-    #println("monoid absorb $add for $Tv: ", monoid_absorb(Tv, add))
-    # No mask
-    if mask === nothing
-        kernel! = csr_spmv_kernel!(backend)
-        kernel!(
-            C,
-            A.rowptr,
-            A.colval,
-            A.nzval,
-            B,
-            monoid_neutral(promote_type(Tv, InputType), add),
-            monoid_absorb(promote_type(Tv, InputType), add),
-            mul,
-            add,
-            accum;
-            ndrange = size(A, 1),
+    # Check types
+    if !(promote_type(Tv, InputType) <: ResType)
+        throw(
+            ArgumentError(
+                "Result type must be able to hold the result of the multiplication",
+            ),
         )
-        return
     end
-    # Check mask type
-    if !(typeof(mask) <: AbstractVector{Tmask})
-        throw(DimensionMismatch("Mask must be a vector"))
+    # Check backends
+    backend = get_backend(A)
+    if get_backend(B) != backend || get_backend(C) != backend
+        throw(ArgumentError("All inputs must be on the same backend"))
     end
-    # Check mask length
-    if length(mask) != size(A, 1)
-        throw(DimensionMismatch("Mask length must be equal to the number of rows in A"))
-    end
-    # Check mask backend
-    if get_backend(mask) != backend
-        throw(ArgumentError("Mask must be on the same backend as A"))
-    end
-
-    # SparseVector mask 
-    if typeof(mask) <: AbstractSparseGPUVector{Tmask,Ti}
-        kernel! = sparse_masked_csr_spmv_kernel!(backend)
-        kernel!(
-            C,
-            A.rowptr,
-            A.colval,
-            A.nzval,
-            B,
-            monoid_neutral(promote_type(Tv, InputType), add),
-            monoid_absorb(promote_type(Tv, InputType), add),
-            mask.nzind,
-            mul,
-            add,
-            accum;
-            ndrange = nnz(mask),
-        )
-        return
-    end
-
-    # DenseVector mask
-    if typeof(mask) <: AbstractVector{Tmask}
-        if add == GPUGraphs_any
-            #println("Using any_dense_masked_csr_spmv_kernel!")
-            kernel! = any_dense_masked_csr_spmv_kernel!(backend)
-            kernel!(
-                C,
-                A.rowptr,
-                A.colval,
-                A.nzval,
-                B,
-                mask,
-                zero(Tmask),
-                mul,
-                accum;
-                ndrange = size(A, 1),
-            )
-            return
+    # Check mask if provided
+    if mask !== nothing
+        # Check mask type
+        if !(typeof(mask) <: AbstractVector{Tmask})
+            throw(DimensionMismatch("Mask must be a vector"))
         end
-        kernel! = dense_masked_csr_spmv_kernel!(backend)
-        kernel!(
-            C,
-            A.rowptr,
-            A.colval,
-            A.nzval,
-            B,
-            monoid_neutral(promote_type(Tv, InputType), add),
-            monoid_absorb(promote_type(Tv, InputType), add),
-            mask,
-            zero(Tmask),
-            mul,
-            add,
-            accum;
-            ndrange = size(A, 1),
-        )
-        return
+        # Check mask length
+        if length(mask) != size(A, 1)
+            throw(DimensionMismatch("Mask length must be equal to the number of rows in A"))
+        end
+        # Check mask backend
+        if get_backend(mask) != backend
+            throw(ArgumentError("Mask must be on the same backend as A"))
+        end
     end
-
 end
